26ab5b4decc40227c37f80c30ba4f3747b65e586
Nandhini0123 <112197061+Nandhini0123@users.noreply.github.com>
2023-06-26 17:09:24 -0500

    Added remove function
    1. Will not add the same movie again to watchlist

--- watchlist.js
+++ watchlist.js
@@ -2,6 +2,12 @@ let storedData = JSON.parse(localStorage.getItem("myWatchlist"))
 // let storedData = JSON.parse(localStorage.getItem("movieList"))
 console.log(storedData)
 
+document.addEventListener("click", function(e){
+   if(e.target.dataset.id){
+      removeMovie(e.target.dataset.id)
+   }
+})
+
 function displayWatchlist(){
     for(let i = 0; i < storedData.length; i++){
     fetch(`http://www.omdbapi.com/?i=${storedData[i]}&apikey=ed3e63ef`)
@@ -22,7 +28,7 @@ function displayWatchlist(){
                     <p class="genre">${data.Genre}</p>
                  </div>
                  <div class="watch">
-                    <button data-Id="${data.imdbID}" class="watchlist"><i class="fa-solid fa-circle-plus"></i>  watchlist</button>
+                    <button data-Id="${data.imdbID}" class="watchlist"><i class="fa-solid fa-circle-minus"></i>  remove</button>
                  </div>
                  <div class="third-line">
                     <p class="plot">${data.Plot}</p>
@@ -33,4 +39,20 @@ function displayWatchlist(){
     }
 }
 
+
+
+function removeMovie(imdbID){
+  console.log(storedData)
+  let moviesInLocal = []; 
+  let indexToRemove = storedData.indexOf(imdbID);
+  storedData.splice(indexToRemove, 1)
+  moviesInLocal = storedData;
+ 
+  console.log("OK", moviesInLocal)
+  localStorage.clear();
+  localStorage.setItem("myWatchlist", JSON.stringify(moviesInLocal))
+  storedData = JSON.parse(localStorage.getItem("myWatchlist"))
+  console.log("final",storedData)
+  location.reload()
+}
 displayWatchlist()
